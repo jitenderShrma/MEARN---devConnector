633bc295d27ac95826ef7f7fa711bd609001ed0d
jitenderKumar97 <jitenderkanti020@gmail.com>
2019-02-24 21:12:24 +0530

    with env.vars

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,5 @@
-// if(process.env.NODE_ENV === 'production'){
-//   module.exports = require('./keys_prod');
-// } else {
-//   module.exports = require('./keys_dev');
-// }
-module.exports = {
-  mongoURI: 'mongodb://jit:jitender1@ds155292.mlab.com:55292/mydevonnector'
+if(process.env.NODE_ENV === 'production'){
+  module.exports = require('./keys_prod');
+} else {
+  module.exports = require('./keys_dev');
 }
